02edbf982ab01de909b8e4fbedce4f13d2c174d4
Johnathan Rhoades <jrhoades7989@gmail.com>
2026-09-09 21:34:23 -0400

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.go
@@ -0,0 +1,17 @@
+func isAnagram(s string, t string) bool {
+    if len(s) != len(t){
+        return false
+    }
+
+    var count [26]int
+    for i := range s{
+        count[s[i]-'a']++
+        count[t[i]-'a']--
+    }
+    for _,c := range count {
+        if c!=0 {
+            return false
+        }
+    }
+    return true
+}
